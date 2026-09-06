f45a065c6206803fe929443382e69d37a1f803e3
Octavio Montt <montt.octavio@gmail.com>
2026-09-06 22:40:42 +0100

    move reg to r_m

---
+++ 1_1/mov.py
@@ -0,0 +1,49 @@
+reg_map_word = {
+    0b000: "ax",
+    0b001: "cx",
+    0b010: "dx",
+    0b011: "bx",
+    0b100: "sp",
+    0b101: "bp",
+    0b110: "si",
+    0b111: "di",
+}
+
+reg_map_byte = {
+    0b000: "al",
+    0b001: "cl",
+    0b010: "dl",
+    0b011: "bl",
+    0b100: "ah",
+    0b101: "ch",
+    0b110: "dh",
+    0b111: "bh",
+}
+
+OPCODE_MOV_REG_RM = 0b100010
+
+with open("mov_ex_2", "rb") as f:
+    print("; This is the python test")
+    print("bits 16")
+    while instruction := f.read(2):
+        if len(instruction) != 2:
+            break
+
+        first_byte, second_byte = instruction[0], instruction[1]
+
+        opcode = (first_byte >> 2) & 0b111111
+        d = (first_byte >> 1) & 0b1
+        w = first_byte & 0b1
+
+        mod = (second_byte >> 6) & 0b11
+        reg = (second_byte >> 3) & 0b111
+        r_m = second_byte & 0b111
+
+        # MOV
+        if opcode == OPCODE_MOV_REG_RM:
+            assert mod == 0b11, (
+                f"Only register-to-register mode supported so far (got mod={mod:02b})"
+            )
+            reg_map = reg_map_word if w else reg_map_byte
+            dest, src = (reg, r_m) if d else (r_m, reg)
+            print(f"mov {reg_map[dest]}, {reg_map[src]}")
